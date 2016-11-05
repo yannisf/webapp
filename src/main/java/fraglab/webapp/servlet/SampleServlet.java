7190346f38598f7291512d9986dec83c0d965457
yannisf <yannisf@ianos>
2016-11-05 22:08:16 +0200

    Postgres commit

--- src/main/java/fraglab/webapp/servlet/SampleServlet.java
+++ src/main/java/fraglab/webapp/servlet/SampleServlet.java
@@ -1,16 +1,18 @@
 package fraglab.webapp.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/sample")
 public class SampleServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         DatabaseHelper helper = new DatabaseHelper();
-        resp.getWriter().println("Received GET request: " + helper.getMessage());
+        resp.getWriter().println("Received GET request: " + helper.getVersion());
     }
 }
--- src/test/java/fraglab.webapp.servlet/DatabaseHelperTest.java
+++ src/test/java/fraglab.webapp.servlet/DatabaseHelperTest.java
@@ -1,29 +1,16 @@
 package fraglab.webapp.servlet;
 
+import org.testng.annotations.Test;
+
 import java.sql.*;
 import java.util.Properties;
 
-class DatabaseHelper {
+class DatabaseHelperTest {
 
-    String getVersion() {
-        String version = "N/A";
-        String url = "jdbc:postgresql://localhost/registry_db";
-        Properties props = new Properties();
-        props.setProperty("user", "registry_db_user");
-        props.setProperty("password", "registry_db_user");
-        try (
-                Connection connection = DriverManager.getConnection(url, props);
-                Statement statement = connection.createStatement();
-        ) {
-            ResultSet resultSet = statement.executeQuery("select version()");
-            resultSet.next();
-            version = resultSet.getString(1);
-        } catch (SQLException e) {
-            //
-        }
-
-        return version;
+    @Test
+    public void databaseTest() {
+        DatabaseHelper helper = new DatabaseHelper();
+        String version = helper.getVersion();
+        System.out.println(version);
     }
-
-
 }
